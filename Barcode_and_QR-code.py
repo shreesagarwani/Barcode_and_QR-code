9c50fbc2263ef3fb73e8a239b8dd380ae54e2714
shreesagarwani <63024284+shreesagarwani@users.noreply.github.com>
2023-07-12 18:09:04 +0530

    Update Barcode_and_QR-code.py

--- Barcode_and_QR-code.py
+++ Barcode_and_QR-code.py
@@ -10,3 +10,17 @@ def read_barcodes(frame):
         with open("barcode_result.txt", mode ='w') as file:
             file.write("Recognized Barcode:" + barcode_info)
     return frame
+
+def main():
+    camera = cv2.VideoCapture(0)
+    ret, frame = camera.read()
+    while ret:
+        ret, frame = camera.read()
+        frame = read_barcode(frame)
+        cv2.imshow('Barcode/QR code reader', frame)
+        if cv2.waitKey(1) & 0xFF == 27:
+            break
+    camera.release()
+    cv2.destroyAllWindows()
+if __name__ == '__main__':
+    main()
